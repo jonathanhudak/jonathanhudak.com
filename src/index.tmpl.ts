21d6424cf7c5e11e44c285baec32d2fb5ae8da62
Jonathan Hudak <hudak.jonathan@gmail.com>
2023-09-19 20:43:54 -0700

    remove posts.

--- src/index.tmpl.ts
+++ src/index.tmpl.ts
@@ -1,24 +1,25 @@
 import type { PageData } from "lume/core.ts";
 
 export default function ({ search }: PageData) {
-  const posts = search.pages("type=year published=true");
-  console.log(posts);
-  return `
-    <ul>
-      ${posts
-        .sort((a, b) => +b.data.title - +a.data.title)
-        .map(
-          (post) => `<h2>
-            ${
-              post.data.link
-                ? `<a href="${post.data.url}">${post.data.title}</a>`
-                : post.data.title
-            }
-          </h2>
-          ${post.data.summary && `<p>${post.data.summary}</p>`}
-          `
-        )
-        .join("")}
-    </ul>
-  `;
+  return ``;
+  //   const posts = search.pages("type=year published=true");
+  //   console.log(posts);
+  //   return `
+  //     <ul>
+  //       ${posts
+  //         .sort((a, b) => +b.data.title - +a.data.title)
+  //         .map(
+  //           (post) => `<h2>
+  //             ${
+  //               post.data.link
+  //                 ? `<a href="${post.data.url}">${post.data.title}</a>`
+  //                 : post.data.title
+  //             }
+  //           </h2>
+  //           ${post.data.summary && `<p>${post.data.summary}</p>`}
+  //           `
+  //         )
+  //         .join("")}
+  //     </ul>
+  //   `;
 }
